d6d796819c6ba00a25a124e81c0e29e6c67cf35c
11249a281-hub <11249a281@kanchiuniv.ac.in>
2025-11-20 20:09:13 +0530

    Create selecton sort .c

---
+++ selecton sort .c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Selection Sort algorithm
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+
+        // Find the minimum element in unsorted array
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+
+        // Swap the found minimum with the first element
+        if (min_idx != i) {
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
