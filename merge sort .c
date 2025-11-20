2922c6780970c6bb24bc28264981f2844768c4ae
11249a281-hub <11249a281@kanchiuniv.ac.in>
2025-11-20 20:04:20 +0530

    Create merge sort .c

---
+++ merge sort .c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+// Function to merge two subarrays
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1; // size of left subarray
+    int n2 = r - m;     // size of right subarray
+
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    // Merge back to original array
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    // Copy remaining elements, if any
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+// Merge Sort function
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2; // middle index
+        mergeSort(arr, l, m);    // sort left half
+        mergeSort(arr, m + 1, r); // sort right half
+        merge(arr, l, m, r);     // merge sorted halves
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
