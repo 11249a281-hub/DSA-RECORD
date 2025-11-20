f486c85a3a868be6d6a16ce145c7e3324a65d6ec
11249a281-hub <11249a281@kanchiuniv.ac.in>
2025-11-20 19:49:25 +0530

    Create stack using linked list .c

---
+++ stack using linked list .c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+// Push operation
+void push(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    printf("%d pushed onto stack\n", value);
+}
+
+// Pop operation
+void pop() {
+    if (top == NULL) {
+        printf("Stack Underflow! Nothing to pop\n");
+        return;
+    }
+
+    struct Node* temp = top;
+    printf("%d popped from stack\n", top->data);
+    top = top->next;
+    free(temp);
+}
+
+// Peek (Top element)
+void peek() {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Top element is %d\n", top->data);
+    }
+}
+
+// Display stack elements
+void display() {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    struct Node* temp = top;
+    printf("Stack elements: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n--- Stack Using Linked List Menu ---\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Peek\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                peek();
+                break;
+
+            case 4:
+                display();
+                break;
+
+            case 5:
+                return 0;
+
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    }
+}
